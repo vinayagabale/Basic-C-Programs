724830653a7e9fd60b70789fe51fe9d05bf97a53
Vinaya Gabale <vinayagabale0765@gmail.com>
2026-07-16 08:24:50 +0530

    Add value printing example in 01_Value_Printing.c

---
+++ 01_Value_Printing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 20.5;
+    char c = 'A';
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.1f\n", b);
+    printf("Character: %c\n", c);
+
+    return 0;
+}
